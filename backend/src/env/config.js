c27b2d03f50aed3113d3b2dd2d2b3cbd13898f7e
marco cruz <marco24cruz08@gmail.com>
2022-08-31 19:14:00 -0500

    :tada: first commit

---
+++ backend/src/env/config.js
@@ -0,0 +1,26 @@
+const joi = require("joi");
+
+const envVarsSchema = joi
+  .object()
+  .keys({
+    PORT: joi.number().positive().required(),
+    MONGO_URL: joi.string().required(),
+  })
+  .unknown();
+
+const { value: envVars, error } = envVarsSchema
+  .prefs({ errors: { label: "key" } })
+  .validate(process.env);
+
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
+const configEnv = {
+  mongoUrl: envVars.MONGO_URL,
+  port: envVars.PORT,
+};
+
+module.exports = {
+  configEnv,
+};
